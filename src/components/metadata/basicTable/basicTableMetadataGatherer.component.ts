763fccda67caaa9b11776b5d7f7814b15dce4829
kukjevov <kukjevov@gmail.com>
2018-03-15 14:20:02 +0100

    UPDATE - updated documentation for metadata gatherer

--- src/components/metadata/basicTable/basicTableMetadataGatherer.component.ts
+++ src/components/metadata/basicTable/basicTableMetadataGatherer.component.ts
@@ -7,6 +7,54 @@ import {BasicTableColumnComponent} from "./basicTableColumn.component";
 
 /**
  * Component that is used for gathering metadata for basic table
+ *
+ * This is default metadata gatherer which works with `BasicTableColumnComponent`
+ *
+ * Here is sample of use of available options for `BasicTableMetadataGathererComponent` with `BasicTableColumnComponent`
+ *
+ * ``` html
+ * <ng-grid #grid [gridOptions]="gridOptions" class="clearfix" fixedHeader>
+ *     <basic-table-metadata>
+ *         <!-- Basic usage, id must be unique -->
+ *         <!-- if custom template is not provided name is used for getting data -->
+ *         <!-- title is displayed in header of grid -->
+ *         <basic-table-column id="colId" name="colId" title="Id"></basic-table-column>
+ *
+ *         <!-- Tooltip that is displayed on hover on column header -->
+ *         <!-- This column can be used for ordering -->
+ *         <basic-table-column id="colId2" name="colId2" title="Col 2" headerTooltip="This is tooltip text" [ordering]="true"></basic-table-column>
+ *
+ *         <!-- Title in header is no displayed -->
+ *         <!-- Column width set to fixed max 60px -->
+ *         <basic-table-column id="colId3" name="colId3" title="Col 3" [titleVisible]="false" width="60px"></basic-table-column>
+ *
+ *         <!-- Column default behavior will be that it will not be rendered -->
+ *         <basic-table-column id="colId5" name="colId5" title="Col 5" [visible]="false"></basic-table-column>
+ *
+ *         <!-- Column will have custom css class applied to header and each cell -->
+ *         <basic-table-column id="colId6" name="colId6" title="Col 6" headerClass="header-css-class" cellClass="cell-css-class"></basic-table-column>
+ *
+ *         <!-- Column with custom template for header and each cell -->
+ *         <basic-table-column id="colId7" name="colId7" title="Custom col">
+ *             <!-- Custom header template -->
+ *             <ng-template #headerTemplate>
+ *                 <div title="This is custom tooltip">Custom column</div>
+ *             </ng-template>
+ *
+ *             <!-- Custom body cell template -->
+ *             <ng-template #bodyTemplate let-item let-index="index" let-rowIndex="rowIndex" let-column="column" let-startingIndex="startingIndex">
+ *                 <div>
+*                      <div>{{item?.dateItem?.format('L')}} 'item' is row object from data</div>
+*                      <div>{{index}} index is zero based index of row for all items</div>
+*                      <div>{{rowIndex}} rowIndex is zero based index of row for current page</div>
+*                      <div>{{column}} column is object of 'BasicTableColumnComponent'</div>
+*                      <div>{{startingIndex}} startingIndex starting zero based index of current page</div>
+ *                 </div>
+ *             </ng-template>
+ *         </basic-table-column>
+ *     </basic-table-metadata>
+ * </ng-grid>
+ * ```
  */
 @Component(
 {
@@ -51,7 +99,7 @@ export class BasicTableMetadataGathererComponent<TData> implements AfterContentI
     }
 
     //######################### public methods - implementation of AfterContentInit #########################
-    
+
     /**
      * Called when content was initialized
      */
--- src/components/metadata/metadata.interface.ts
+++ src/components/metadata/metadata.interface.ts
@@ -30,6 +30,37 @@ export class METADATA_GATHERER
 
 /**
  * Gatherer used for gathering metadata for grid
+ *
+ * If you need to use your own metadata gatherer you need to implement this interface and `Component` that
+ * implements this interface must provide itself as `METADATA_GATHERER` see example below:
+ *
+ * ``` typescript
+ * (at)Component(
+ * {
+ *     selector: 'ng-grid > your-table-metadata',
+ *     template: '',
+ *     changeDetection: ChangeDetectionStrategy.OnPush,
+ *     providers:
+ *     [
+ *         <ExistingProvider>
+ *         {
+ *             provide: METADATA_GATHERER,
+ *             useExisting: forwardRef(() => YourTableMetadataGathererComponent)
+ *         }
+ *     ]
+ * })
+ * export class YourTableMetadataGathererComponent implements MetadataGatherer<YourMetadataType>
+ * {
+ *     public metadataChange: EventEmitter<void> = new EventEmitter<void>();
+ * 
+ *     getMetadata(): YourMetadataType
+ *     {
+ *         return null;//TODO - implement obtaining metadata
+ *     }
+ * 
+ *     //TODO - do what you need to obtain metadata
+ * }
+ * ```
  */
 export interface MetadataGatherer<TMetadata>
 {
